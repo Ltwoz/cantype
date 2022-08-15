684e2c50e67b0e36d3ef6b3601177508c20edcc9
Ltwoz <warrapat.102@gmail.com>
2022-08-15 21:39:15 +0700

    axios fixed

--- src/utils/misc.js
+++ src/utils/misc.js
@@ -4,15 +4,17 @@ let themeList = [];
 export async function getThemeList() {
     if (themeList.length == 0) {
         return axios.get("/src/themes/_list.json").then((res) => {
-            const list = res.data.sort((a, b) => {
-                const nameA = a.name.toLowerCase();
-                const nameB = b.name.toLowerCase();
-                if (nameA < nameB) return -1;
-                if (nameA > nameB) return 1;
-                return 0;
-            });
-            themeList = list;
-            return themeList;
+            if (res.status == 200) {
+                const list = res.data.sort((a, b) => {
+                    const nameA = a.name.toLowerCase();
+                    const nameB = b.name.toLowerCase();
+                    if (nameA < nameB) return -1;
+                    if (nameA > nameB) return 1;
+                    return 0;
+                });
+                themeList = list;
+                return themeList;
+            }
         });
     } else {
         return themeList;

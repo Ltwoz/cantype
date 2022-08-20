ebcaca2aca42077bbfd7a3dd0e8d15d6b4e53e57
Ltwoz <warrapat.102@gmail.com>
2022-08-20 17:26:31 +0700

    mascot

--- src/components/Mascot.jsx
+++ src/components/Mascot.jsx
@@ -4,17 +4,17 @@ import mascotEyes from "../public/mascotEyes.png";
 import mascotBlush from "../public/mascotBlush.png";
 import { MascotColor, MascotCStroke, MascotShadow } from "./MascotLayer";
 
-
 const Mascot = () => {
     return (
         <div className="mascot">
             <img src={mascotTop} alt="" style={{ zIndex: 2 }} />
             <img src={mascotEyesHL} alt="" style={{ zIndex: 6 }} />
             <img src={mascotEyes} alt="" style={{ zIndex: 5 }} />
-            <img src={mascotBlush} alt="" style={{ zIndex: 4}} />
-            <MascotCStroke fill="#79102a" zIndex={3} />
-            <MascotShadow fill="#79102a" zIndex={3} />
-            <MascotColor fill="#d62436" zIndex={1} />
+            <img src={mascotBlush} alt="" style={{ zIndex: 5}} />
+            <MascotCStroke fill="var(--bg-color)" zIndex={3} />
+            <MascotShadow fill="#333" zIndex={4} opacity={0.3} />
+            <MascotColor fill="var(--hl-color)" zIndex={1} />
+            
         </div>
     );
 };
